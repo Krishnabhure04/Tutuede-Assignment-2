ff37a0f0895af2de0db3c4934c358bf07de90014
Krishna Bhure <krishnabhure2004@gmail.com>
2025-05-12 21:57:43 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+'''
+Task 1: Check if a Number is Even or Odd
+Problem Statement:  Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.
+'''
+a=int(input("enter a value: "))
+if a%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
